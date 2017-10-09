fa8c4ebc9d5163af1c4e6ed30ada212d381885c1
trinidadmiggy <juanmiguel.trinidad@benilde.edu.ph>
2017-10-09 21:42:07 +0800

    9

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,6 +2,7 @@
 
 class Login extends CI_Controller 
 {
+  private $level;
   function __construct()
   {
    parent::__construct();
@@ -22,13 +23,18 @@ class Login extends CI_Controller
   }
   else
   {
-    redirect('applicant/examination/index', 'refresh');
-  }
+    if($this->level == "applicant"){
+     redirect('applicant/examination/index', 'refresh');
+   } else if($this->level == "hr") {
+
+   }
+   redirect('hr/asdasd', 'refresh');
+ }
 
 }
 public function send_email($email) 
 {
-  $emailconfig = Array(
+  $emailconfig = array(
     'protocol' => 'smtp',
     'smtp_host' => 'ssl://smtp.googlemail.com',
     'smtp_port' => 465,
@@ -51,22 +57,41 @@ function check_database($password)
  $email = $this->input->post('email');
 
    //query the database
- $result = $this->login_model->login($email, $password);
+ $applicant = $this->login_model->applogin($email, $password);
+ $employee = $this->login_model->employeelogin($email, $password);
 
- if($result)
+ if($applicant)
  {
-  /*  $this->send_email($email);*/
-  $sess_array = array();
-  foreach($result as $row)
+   $this->level = "applicant";
+   /*  $this->send_email($email);*/
+   $sess_array = array();
+   foreach($applicant as $row)
+   {
+     $sess_array = array(
+      'lname' =>  $row->lname,
+      'fname' =>  $row->fname,
+      'mname' =>  $row->mname,
+      'app_id' => $row->app_id,
+      'email' => $row->email
+    );
+     $this->session->set_userdata('logged_in', $sess_array);
+   }
+   return TRUE;
+ }
+ else if($employee) 
+ {
+  $this->level = "hr";
+  $employee_sess = array();
+  foreach($employee as $row)
   {
-   $sess_array = array(
+   $employee_sess = array(
     'lname' =>  $row->lname,
     'fname' =>  $row->fname,
     'mname' =>  $row->mname,
-    'app_id' => $row->app_id,
+    'employee_id' => $row->employee_id,
     'email' => $row->email
   );
-   $this->session->set_userdata('logged_in', $sess_array);
+   $this->session->set_userdata('employee_login', $employee_sess);
  }
  return TRUE;
 }
@@ -76,7 +101,5 @@ else
  return false;
 }
 }
-
-
 }
 ?>
--- application/models/login_model.php
+++ application/models/login_model.php
@@ -1,12 +1,32 @@
 <?php 
 class Login_model extends CI_Model {
-	function login($email, $password)
+	function applogin($email, $password)
 	{
 		$this -> db -> select('applicantinfo.app_id, applicantinfo.lname, applicantinfo.fname, applicantinfo.mname, applicantlogin.email, applicantlogin.password');
 		$this -> db -> from('applicantlogin');
 		$this -> db -> join('applicantinfo', 'applicantinfo.app_id = applicantlogin.app_id');
 		$this -> db -> where('applicantlogin.email', $email);
 		$this -> db -> where('applicantlogin.password', $password);
+		$this -> db -> limit(1);
+
+		$query = $this -> db -> get();
+
+		if($query -> num_rows() == 1)
+		{
+			return $query->result();
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	function employeelogin($email, $password)
+	{
+		$this -> db -> select('employee_id, lname, fname, mname, email, password');
+		$this -> db -> from('employee');
+		$this -> db -> where('email', $email);
+		$this -> db -> where('password', $password);
 		$this -> db -> limit(1);
 
 		$query = $this -> db -> get();
