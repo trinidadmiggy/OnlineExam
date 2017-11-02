128136f2c47f28f71a3c17efe83f83a7f9ed5090
trinidadmiggy <juanmiguel.trinidad@benilde.edu.ph>
2017-11-02 14:07:53 +0800

    2.1

--- application/controllers/applicant/jobs.php
+++ application/controllers/applicant/jobs.php
@@ -39,25 +39,31 @@ class Jobs extends CI_Controller {
 		$session_data = $this->session->userdata('logged_in');
 		$app_id = $session_data['app_id'];
 
+		$this->form_validation->set_rules('essayAns', 'Essay', 'trim');
+
 		if($this->jobs_model->checkIfTakenExam($app_id)) {
 			if($this->jobs_model->checkApplicantJobs($app_id, $this->input->post('job_id'))) {
 				echo '<script>alert("Uh oh! You already applied for this job");</script>';
 				redirect('applicant/jobs', 'refresh');
 			} else {
-				$essay = array(
-					'app_id' => $app_id, 
-					'question_id' => $this->input->post('essayid'), 
-					'answer' => $this->input->post('essayAns')
-				);
-				$essay_id = $this->jobs_model->answerEssay($essay);
-				$job = array(
-					'app_id' => $app_id,
-					'job_id' => $this->input->post('job_id'),
-					'essay_id' => $essay_id
-				);
-				$this->jobs_model->applyJob($job);
-				echo '<script>alert("Thank you for applying at Questronix");</script>';
-				redirect('applicant/jobs', 'refresh');
+				if($this->form_validation->run()) {
+					$essay = array(
+						'app_id' => $app_id, 
+						'question_id' => $this->input->post('essayid'), 
+						'answer' => $this->input->post('essayAns')
+					);
+					$essay_id = $this->jobs_model->answerEssay($essay);
+					$job = array(
+						'app_id' => $app_id,
+						'job_id' => $this->input->post('job_id'),
+						'essay_id' => $essay_id
+					);
+					$this->jobs_model->applyJob($job);
+					echo '<script>alert("Thank you for applying at Questronix");</script>';
+					redirect('applicant/jobs', 'refresh');
+				} else {
+					$this->session->set_flashdata('error', validation_errors());
+				}
 			}
 		} else {
 			echo "<script>alert('Uh oh! Please take the examination first...');</script>";
